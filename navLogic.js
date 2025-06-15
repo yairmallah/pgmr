1822c0c8072e66f0d97ffa92591cdb6ae688a3ac
yairmallah <yair.mallah@gmail.com>
2025-06-15 19:52:48 +0300

    Update navLogic.js

--- navLogic.js
+++ navLogic.js
@@ -1,6 +1,7 @@
 import * as dispMode from './displayMode.js';
 
 document.addEventListener("DOMContentLoaded", function () {
+	console.log("start");
 	var isInIframe = false;
 	try {
 		isInIframe = window.self !== window.top;
